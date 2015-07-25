b0c6cdb467cf6b2fbc6e54b74888363ecf1129ad
Xavier Damman <xdamman@gmail.com>
2015-07-24 22:37:34 -0300

    mediumexporter bin

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
+#! /usr/bin/env node
+
 var utils = require('./utils')
   ;
 
+
+var mediumURL = process.argv[2];
+
+if(!mediumURL || !mediumURL.match(/^http/i)) {
+  console.log("Usage: mediumexporter MediumURL\n");
+  console.log("This will fetch the medium post and convert it to markdown to stdout.");
+  console.log("e.g. $> mediumexporter https://medium.com/@xdamman/my-10-day-meditation-retreat-in-silence-71abda54940e > medium_post.md");
+  process.exit(0);
+}
+
+// For testing
+/*
 var mediumURL = "https://medium.com/@xdamman/my-10-day-meditation-retreat-in-silence-71abda54940e";
 var mediumURL = "./test/vipassana.json";
+*/
 
 utils.loadMediumPost(mediumURL, function(err, json) {
 
