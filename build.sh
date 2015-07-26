9e621a5547bad33322763571ce1da4bafb1686b9
Xavier Damman <xdamman@gmail.com>
2015-07-26 00:32:42 -0300

    added examples

--- build.sh
+++ build.sh
@@ -3,6 +3,5 @@
 for MEDIUM_POST in `ls test/medium_posts/`; do
   SLUG=`echo $MEDIUM_POST | sed 's/.json//g'`
   echo "mediumexporter ./test/medium_posts/${SLUG}.json > examples/${SLUG}.md"
-  #mediumexporter ./test/medium_posts/${SLUG}.json > examples/${SLUG}.md
-  sleep 1;
+  mediumexporter ./test/medium_posts/${SLUG}.json > examples/${SLUG}.md
 done;
