f13fad59faf21bd3c9490301339aa78c90bf6f6c
Travis CI <bobby.brennan@gmail.com>
2017-10-16 15:05:46 +0000

    create entry.js

--- get-feed.js
+++ get-feed.js
@@ -6,17 +6,18 @@ var rssParser = require('rss-parser')
 module.exports = function(feedURL, program, callback) {
   let outputDir = program.output || process.cwd();
   rssParser.parseURL(feedURL, function(err, data) {
+    if (err) return callback(err);
     Promise.all(data.feed.entries.map(entry => {
       return new Promise((resolve, reject) => {
         getPost(entry.link, program, function(err, text) {
           if (err) return reject(err);
-          let filename = path.join(outputDir, entry.title.replace(/\W+/g, '_') + '.md');
-          fs.writeFile(filename, text, err => {
+          entry.filename = path.join(outputDir, entry.title.replace(/\W+/g, '_') + '.md');
+          fs.writeFile(entry.filename, text, err => {
             if (err) return reject(err);
             resolve(text);
           })
         });
       })
-    })).then(_ => callback && callback());
+    })).then(_ => callback && callback(null, data));
   })
 }
